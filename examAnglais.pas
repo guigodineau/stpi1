2d05cbc6cfcef7a8863b8d814d293fab714bd3ae
guigodineau <72358483+guigodineau@users.noreply.github.com>
2020-10-14 21:35:01 +0200

    procedure exam anglais

---
+++ examAnglais.pas
@@ -0,0 +1,37 @@
+procedure lectureFichierAng( var mot, traduction : String );
+var fichier : Text;
+    i, j : Integer;
+begin
+randomize;
+j := 1;
+i := random(9) + 1;
+assign ( fichier, 'anglais.pas' );
+reset ( fichier );
+for j := 1 to i-1 do
+  readln ();
+read( fichier, mot);
+read( fichier, traduction);
+close( fichier);
+end;
+
+procedure choixJoueurAng( mot : string; var proposition : String);
+begin
+write( mot, ' : ' );
+read( proposition);
+end;
+
+function estJusteAngl( proposition, traduction : string ) : Boolean;
+begin
+estJusteAngl := false;
+if proposition = traduction then
+  estJusteAngl := true;
+end;
+
+procedure examAnglais( var gagneAngl : boolean );
+var mot, traduction, proposition : String;
+begin
+affichageTexte( 'examAnglais' );
+lectureFichierAng( mot, traduction);
+choixJoueurAng( mot, proposition);
+gagneAngl := (estJusteAngl( proposition, traduction ));
+end;
