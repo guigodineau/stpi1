1627d7f5ae1803ba8384b436e91f76f6b7643d97
guigodineau <72358483+guigodineau@users.noreply.github.com>
2020-10-16 12:17:46 +0200

    crea unité miniJeux

--- principal.pas
+++ principal.pas
@@ -1,21 +1,5 @@
 program principal;
-
-procedure affichageTexte (nomEpreuve : string);
-var fichier : Text;
-    mot, nomFichier : String;
-
-begin
-  nomFichier := nomEpreuve +'.txt';
-  assign (fichier, nomFichier );
-  reset (fichier);
-  while not Eof(fichier) do
-    begin
-      readln (fichier, mot);
-      write (mot);
-      writeln();
-    end;
-  close(fichier);
-end;
+uses miniJeux;
 
 begin
 
